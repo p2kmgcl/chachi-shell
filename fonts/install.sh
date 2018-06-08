a059426fd4d45536c5ffeb1c80e4591ad4ebb4dc
Pablo Molina <contact@pablomolina.me>
2018-06-08 09:42:58 +0200

    Change font

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo Copying Fira Code font
+echo Copying Google Fonts
 
-if [ ! -d /usr/share/fonts/truetype/fira-code ]; then
-  git clone -q git@github.com:tonsky/FiraCode.git /tmp/fira-code
-  sudo mkdir -p /usr/share/fonts/truetype/fira-code
-  sudo cp /tmp/fira-code/distr/ttf/* /usr/share/fonts/truetype/fira-code/
-  sudo rm -r /tmp/fira-code/distr/ttf/
+if [ ! -d /usr/share/fonts/truetype/google-fonts ]; then
+  git clone -q git@github.com:google/fonts.git /tmp/google-fonts
+  sudo mkdir -p /usr/share/fonts/truetype/google-fonts
+  sudo cp /tmp/google-fonts/ofl/inconsolata/*.ttf /usr/share/fonts/truetype/google-fonts/
+  sudo rm -r /tmp/google-fonts
 fi
