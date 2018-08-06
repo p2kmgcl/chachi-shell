c49a3fa5654d193dc9410696bb5340a17f0f4689
Pablo Molina <contact@pablomolina.me>
2018-08-06 10:19:02 +0200

    font

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 echo Updating repositories
 sudo apt-get -q -y update
 sudo apt-get install -q -y git git-extras vim vim-gtk wget build-essential cmake powerline-fonts python3-dev python-dev
-sudo pacman -Syy base-devel cmake git vim wget
-yaourt -Syy powerline-fonts-git
+sudo pacman -Syy base-devel cmake git ttf-anonymous-pro ttf-fira-code ttf-fira-mono ttf-fira-sans vim wget
 clear
 
 ./bash/install.sh
