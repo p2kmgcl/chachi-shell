85f5cf9e859ae75f52604d0cd56bba6b7bd84479
Pablo Molina <contact@pablomolina.me>
2018-09-03 15:04:18 +0200

    update

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 echo Updating repositories
 sudo apt-get -q -y update
-sudo apt-get install -q -y git git-extras vim vim-gtk wget build-essential cmake powerline-fonts python3-dev python-dev
-sudo pacman -Syy base-devel cmake git ttf-anonymous-pro ttf-fira-code ttf-fira-mono ttf-fira-sans vim wget
+sudo apt-get install -q -y bat git git-extras vim vim-gtk wget build-essential cmake powerline-fonts python3-dev python-dev
+sudo pacman -Syy bat base-devel cmake git ttf-anonymous-pro ttf-fira-code ttf-fira-mono ttf-fira-sans vim wget
 clear
 
 ./bash/install.sh
