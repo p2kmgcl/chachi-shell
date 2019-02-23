41b9285b7a114787c20fb56a0381af94f78fa34a
Pablo Molina <contact@pablomolina.me>
2019-02-23 09:30:49 +0100

    Update fzf

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-export FZF_DEFAULT_COMMAND='rg --color=never --files --glob "!.git/*,!.gradle/*,!.hg/*,!.sass-cache/*,!.svn/*,!bower_components/*,!build/*,!classes/*,!CVS/*,!node_modules/*,!tmp/*"'
+export FZF_DEFAULT_COMMAND='rg --color=never --files'
 
-function cdd() {
-  DIR=$(rg --color=never --files --null --glob '!{.git,.gradle,.hg,.sass-cache,.svn,bower_components,build,classes,CVS,node_modules,tmp}/*' ~ | xargs -0 dirname | sort -u | fzf)
-  echo $DIR
-  cd $DIR
-}
-
+alias ..='cd ..'
+alias cdd='cd $(f)'
+alias codee='code $(f)'
+alias f="${FZF_DEFAULT_COMMAND} --null ~ | xargs -0 dirname | sort -u | fzf --color=16"
 alias ls='ls --color=auto'
 alias ll='ls --color=auto -lh'
 alias lla='ls --color=auto -lha'
-alias ..='cd ..'
 alias serve='python -m http.server 8080'
