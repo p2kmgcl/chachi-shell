d31dabb9fbb83bfa1cd973645e97ff6c2dfd557c
Pablo Molina <contact@pablomolina.me>
2019-03-25 14:10:47 +0100

    Update

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 echo Updating repositories
 sudo apt-get -q -y update
-sudo apt-get install -q -y cmake git git-extras vim vim-gtk wget build-essential cmake python3-dev python-dev
-sudo pacman -Syy bat base-devel cmake git ttf-anonymous-pro ttf-fira-code ttf-fira-mono ttf-fira-sans vim wget
+sudo apt-get install -q -y cmake git git-extras vim vim-gtk wget build-essential cmake python3-dev python-dev terminator
+sudo pacman -Syy bat base-devel cmake git terminator ttf-anonymous-pro ttf-fira-code ttf-fira-mono ttf-fira-sans vim wget
 clear
 
 ./bash/install.sh
