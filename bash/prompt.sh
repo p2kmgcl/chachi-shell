70b0e1300a76f3c0a12e639f45220d3582367c68
Pablo Molina <contact@pablomolina.me>
2019-04-02 11:51:27 +0200

    Move to dark

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+prompt_newline() {
+  echo -e '\n'
+}
+
 prompt_user() {
-  echo -e "\n: `whoami`"
+  echo -e " `whoami`"
 }
 
 prompt_dir() {
-  echo -e "\n: `realpath $(pwd)`"
+  echo -e " `realpath $(pwd)`"
 }
 
 prompt_gits() {
   if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
-    echo -e "\n: `git status -s -b | head -1 | cut -c4-100`"
+    echo -e " (`git branch | grep \* | cut -d ' ' -f2`)"
   fi
 }
 
-export PS1="\[\033[35m\]\$(prompt_dir) \$(prompt_gits) \$(prompt_user)\n: \[\033[00m\]"
+export PS1="\[\033[1;34m\]\$(prompt_newline):\$(prompt_gits)\$(prompt_dir)\n: \[\033[00m\]"
