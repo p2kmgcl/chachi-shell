f57c7d69184ea8fd5f378c97dc13fbf1f3c9ff32
Pablo Molina <contact@pablomolina.me>
2020-01-21 11:10:14 +0100

    Update ansible install script

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ if hash ansible 2>/dev/null; then
   echo Ansible already installed
 else
   echo Installing ansible
-  sudo apt-get update -y 1> /dev/null
-  sudo apt-get install -y software-properties-common curl git 1> /dev/null
-  sudo apt-add-repository -y ppa:ansible/ansible 1> /dev/null
-  sudo apt-get update -y 1> /dev/null
+  sudo apt-get update -y
+  sudo apt-get install -y software-properties-common curl git
+  sudo apt-add-repository -y ppa:ansible/ansible
+  sudo apt-get update -y
+  sudo apt-get install ansible
 fi
