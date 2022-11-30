1c4d48a9863bdaf983e9d4d5a029d5c939a13572
Pablo Molina <contact@pablomolina.me>
2022-11-30 08:19:40 +0100

    Update seniore install script

--- seniore/install.sh
+++ seniore/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cargo install cargo-watch
-sudo apt-get install libssl-dev
+# sudo apt-get install libssl-dev
 sudo cp assets/seniore.png /usr/share/icons/hicolor/512x512/apps/seniore.png
