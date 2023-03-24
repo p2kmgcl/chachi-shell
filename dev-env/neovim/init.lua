bb511c661fbf396c4828bf6c81a6ef97d023c309
Pablo Molina <contact@pablomolina.me>
2023-03-24 14:38:31 +0100

    neovim treesiter lang list

--- dev-env/neovim/init.lua
+++ dev-env/neovim/init.lua
@@ -279,17 +279,17 @@ vim.keymap.set('n', '<leader>sd',
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'javascript', 'lua', 'rust', 'tsx', 'typescript', 'help', 'vim' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<c-backspace>',
+      init_selection = '<leader>cc',
+      node_incremental = '<leader>cN',
+      scope_incremental = '<leader>cM',
+      node_decremental = '<leader>cn',
     },
   },
   textobjects = {
