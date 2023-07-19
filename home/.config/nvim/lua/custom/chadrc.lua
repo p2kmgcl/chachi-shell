ce0ab29baa81646467298987d6e427b95749ca60
Pablo Molina <contact@pablomolina.me>
2023-07-19 10:35:06 +0200

    neovim theme

--- home/.config/nvim/lua/custom/chadrc.lua
+++ home/.config/nvim/lua/custom/chadrc.lua
@@ -4,8 +4,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "github_light",
-  theme_toggle = { "github_light", "one_light" },
+  theme = "onenord_light",
+  theme_toggle = { "onenord_light", "one_light" },
   hl_override = highlights.override,
   hl_add = highlights.add,
 }
