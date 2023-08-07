9918c6875c1e9c5097d508ccaf1ea997d5d849f6
Pablo Molina <contact@pablomolina.me>
2023-08-07 17:46:00 +0200

    seniore liferay

--- seniore/src/commands/liferay.rs
+++ seniore/src/commands/liferay.rs
@@ -1,48 +1,48 @@
 use crate::util::{command, tmp};
-use std::{env, error, fs, path, time, u128};
+use std::{env, fs, path, time};
 
 pub fn build_lang() {
     command::run(
-        &get_portal_item_path("/modules/apps/portal-language/portal-language-lang/"),
+        &get_module_path("portal-language-lang").unwrap(),
         &get_portal_item_path("/gradlew"),
         &vec!["formatSource"],
     );
 
     command::run(
-        &get_portal_item_path("/modules/apps/portal-language/portal-language-lang/"),
+        &get_module_path("portal-language-lang").unwrap(),
         &get_portal_item_path("/gradlew"),
         &vec!["buildLang"],
     );
 
     command::run(
-        &get_portal_item_path("/modules/apps/portal-language/portal-language-lang/"),
+        &get_module_path("portal-language-lang").unwrap(),
         &get_portal_item_path("/gradlew"),
-        &vec!["clean", "deploy", "-Dbuild=portal"],
+        &vec![
+            "clean",
+            "deploy",
+            "-Dbuild=portal",
+            "-Dnodejs.node.env=development",
+        ],
     );
 }
 
-pub fn deploy_updated_modules() {
-    let updated_modules = get_updated_modules();
+pub fn get_module_path(module: &str) -> Option<path::PathBuf> {
+    let portal_path = env::var("LIFERAY_PORTAL_PATH").expect("LIFERAY_PORTAL_PATH env variable");
 
-    println!("Found {} module(s).", updated_modules.len());
-
-    for module_path in updated_modules {
-        command::run(
-            &module_path,
-            &get_portal_item_path("/gradlew"),
-            &vec!["clean", "deploy", "-Dbuild=portal"],
-        );
+    if module.starts_with(portal_path.as_str()) {
+        return Some(path::PathBuf::from(module));
+    } else {
+        for module_path in get_module_list() {
+            if module_path.to_str().unwrap().contains(module) {
+                return Some(module_path);
+            }
+        }
     }
 
-    println!("All modules deployed.");
+    None
 }
 
-pub fn update_modules_cache() {
-    let updated_modules = get_updated_modules();
-    println!("{} module(s) updated.", updated_modules.len());
-}
-
-fn get_updated_modules() -> Vec<path::PathBuf> {
+pub fn get_module_list() -> Vec<path::PathBuf> {
     fn is_osgi_module(directory_path: &path::PathBuf) -> bool {
         fs::read_dir(directory_path).map_or(false, |mut children| {
             children.any(|child_result| {
@@ -53,53 +53,125 @@ fn get_updated_modules() -> Vec<path::PathBuf> {
         })
     }
 
-    fn file_duration(file_path: &path::PathBuf) -> Result<u128, Box<dyn error::Error>> {
-        Ok(fs::metadata(file_path)?
-            .modified()?
-            .duration_since(time::SystemTime::UNIX_EPOCH)?
-            .as_millis())
-    }
+    let mut modules: Vec<path::PathBuf> = Vec::new();
 
-    fn find_updated_modules(directory_path: &path::PathBuf) -> Vec<path::PathBuf> {
-        let mut updated_modules: Vec<path::PathBuf> = Vec::new();
+    fn add_modules(modules: &mut Vec<path::PathBuf>, basedir: &path::PathBuf) {
+        let children = fs::read_dir(basedir).unwrap();
 
-        let children = fs::read_dir(directory_path)
-            .unwrap()
-            .map(|child_result| child_result.unwrap());
-
-        for child in children {
+        for child in children.flatten() {
             if child.file_type().unwrap().is_dir() {
-                let module_path = child.path();
-
-                for child_module in find_updated_modules(&module_path) {
-                    updated_modules.push(child_module);
-                }
-
-                if is_osgi_module(&module_path) {
-                    let duration = {
-                        let mut src_path = module_path.to_path_buf();
-                        src_path.push("src");
-                        file_duration(&src_path).unwrap_or(u128::MAX)
-                    };
-
-                    let module_name = module_path.to_str().unwrap().replace('/', "-");
-                    let tmp_file_name = format!("liferay-module-cache-{}", module_name);
-
-                    let latest_duration =
-                        tmp::read_file(tmp_file_name.as_str()).unwrap_or("".to_string());
-
-                    if duration.to_string() != latest_duration {
-                        updated_modules.push(module_path);
-                        tmp::write_file(tmp_file_name.as_str(), duration.to_string()).unwrap();
-                    }
+                add_modules(modules, &child.path());
+                if is_osgi_module(&child.path()) {
+                    modules.push(child.path());
                 }
             }
         }
-
-        updated_modules
     }
 
-    find_updated_modules(&get_portal_item_path("/modules/apps"))
+    add_modules(&mut modules, &get_portal_item_path("/modules/apps"));
+
+    modules
+}
+
+pub fn deploy_modules(modules: &Vec<String>) {
+    for module in modules {
+        let module_path = get_module_path(module.as_str()).unwrap();
+        let module_name = get_module_name(&module_path);
+
+        command::run(
+            &module_path,
+            &get_portal_item_path("/gradlew"),
+            &vec!["clean", "deploy", "-Dbuild=portal"],
+        );
+
+        tmp::write_file(&module_name, get_module_duration(&module_path).to_string()).unwrap();
+    }
+}
+
+pub fn format_modules(modules: &Vec<String>) {
+    for module in modules {
+        command::run(
+            &get_module_path(module.as_str()).unwrap(),
+            &get_portal_item_path("/gradlew"),
+            &vec!["formatSource"],
+        );
+    }
+}
+
+pub fn update_modules_cache() {
+    for module_path in get_updated_modules() {
+        let module_name = get_module_name(&module_path);
+        tmp::write_file(&module_name, get_module_duration(&module_path).to_string()).unwrap();
+        println!("{}", module_path.to_str().unwrap());
+    }
+}
+
+fn get_module_name(module_path: &path::Path) -> String {
+    format!(
+        "liferay-module-name--{}",
+        module_path.to_str().unwrap().split('/').last().unwrap()
+    )
+}
+
+fn get_directory_duration(directory_path: &path::PathBuf) -> u128 {
+    let children = fs::read_dir(directory_path);
+
+    if children.is_err() {
+        return u128::MIN;
+    }
+
+    let mut directory_duration = u128::MIN;
+
+    for child in children.unwrap().flatten() {
+        let file_type = child.file_type().unwrap();
+
+        let child_duration = if file_type.is_file() {
+            fs::metadata(child.path())
+                .unwrap()
+                .modified()
+                .unwrap()
+                .duration_since(time::SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_millis()
+        } else {
+            get_directory_duration(&child.path())
+        };
+
+        if child_duration > directory_duration {
+            directory_duration = child_duration;
+        }
+    }
+
+    directory_duration
+}
+
+fn get_module_duration(module_path: &path::Path) -> u128 {
+    let mut src_path = module_path.to_path_buf();
+    src_path.push("src");
+    get_directory_duration(&src_path)
+}
+
+fn get_updated_modules() -> Vec<path::PathBuf> {
+    let mut updated_modules = Vec::new();
+
+    for module_path in get_module_list() {
+        let duration = get_module_duration(&module_path);
+
+        let latest_duration =
+            tmp::read_file(get_module_name(&module_path).as_str()).unwrap_or("".to_string());
+
+        if duration.to_string() != latest_duration {
+            updated_modules.push(module_path);
+        }
+    }
+
+    updated_modules
+}
+
+pub fn print_updated_modules() {
+    for module_path in get_updated_modules() {
+        println!("{}", module_path.to_str().unwrap());
+    }
 }
 
 fn get_portal_item_path(item_path: &str) -> path::PathBuf {
--- seniore/src/main.rs
+++ seniore/src/main.rs
@@ -8,38 +8,38 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let some_f: Option<fn()> = match (args[1]).as_str() {
+    match (args[1]).as_str() {
         "liferay" => match (args[2]).as_str() {
-            "build-lang" => Some(liferay::build_lang),
-            "deploy-updated-modules" => Some(liferay::deploy_updated_modules),
-            "update-modules-cache" => Some(liferay::update_modules_cache),
-            _ => None,
+            "build-lang" => liferay::build_lang(),
+            "format-modules" => liferay::format_modules(&args[3..].to_vec()),
+            "deploy-modules" => liferay::deploy_modules(&args[3..].to_vec()),
+            "get-updated-modules" => liferay::print_updated_modules(),
+            "update-modules-cache" => liferay::update_modules_cache(),
+            _ => print_help(),
         },
         "linux" => match (args[2]).as_str() {
-            "get-brightness" => Some(linux::get_brightness),
-            "get-date" => Some(linux::get_date),
-            "get-volume" => Some(linux::get_volume),
-            _ => None,
+            "get-brightness" => linux::get_brightness(),
+            "get-date" => linux::get_date(),
+            "get-volume" => linux::get_volume(),
+            _ => print_help(),
         },
         "woffu" => match (args[2]).as_str() {
-            "get-status" => Some(woffu::get_status),
-            "toggle" => Some(woffu::toggle),
-            _ => None,
+            "get-status" => woffu::get_status(),
+            "toggle" => woffu::toggle(),
+            _ => print_help(),
         },
-        _ => None,
-    };
-
-    if let Some(f) = some_f {
-        f();
-    } else {
-        println!("Unknown command");
-
-        let help_path = home_dir()
-            .expect("home dir")
-            .join("Projects/chachi-shell/seniore/docs/help.txt");
-
-        let help_string = fs::read_to_string(help_path).expect("help file");
-
-        println!("\n{}", help_string);
+        _ => print_help(),
     }
+}
+
+fn print_help() {
+    println!("Unknown command");
+
+    let help_path = home_dir()
+        .expect("home dir")
+        .join("Projects/chachi-shell/seniore/docs/help.txt");
+
+    let help_string = fs::read_to_string(help_path).expect("help file");
+
+    println!("\n{}", help_string);
 }
