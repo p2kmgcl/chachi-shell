9fbefc72464dee91d59c0d88674b9f96e38bf461
Pablo Molina <contact@pablomolina.me>
2023-08-23 14:15:17 +0200

    gradlew

--- liferay/ts-bin/deployModules.ts
+++ liferay/ts-bin/deployModules.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env -S deno run --allow-env --allow-read --allow-run
 
+import { resolvePath } from './util/resolvePath.ts';
 import { runCommandInModules } from './util/runCommandInModules.ts';
 
-await runCommandInModules(Deno.args, 'gw', [
+await runCommandInModules(Deno.args, resolvePath('gradlew'), [
   'clean',
   'deploy',
   '-Dbuild=portal',
--- liferay/ts-bin/formatModules.ts
+++ liferay/ts-bin/formatModules.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S deno run --allow-env --allow-read --allow-run
 
+import { resolvePath } from './util/resolvePath.ts';
 import { runCommandInModules } from './util/runCommandInModules.ts';
 
-await runCommandInModules(Deno.args, 'gw', ['formatSource']);
+await runCommandInModules(Deno.args, resolvePath('gradlew'), ['formatSource']);
