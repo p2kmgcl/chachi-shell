0bb367cc81c2c6901d360aa342fc83e7fb7f18e2
Pablo Molina <contact@pablomolina.me>
2023-12-21 10:52:28 +0100

    disable nvim autoformat

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.autoformat = false
+
 local opt = vim.opt
 
 opt.grepprg = "rg --vimgrep --hidden"
