bbafd3242b0f81e629e1e11fcdf9abf6c58d17e1
Pablo Molina <contact@pablomolina.me>
2024-01-30 17:05:53 +0100

    vim keymaps util

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- local Util = require("lazyvim.util")
+
 vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_bcommits<cr>", { desc = "buffer commits" })
 vim.keymap.set("n", "<leader>fd", "<cmd>DevdocsOpen<cr>", { desc = "Find devdocs" })
