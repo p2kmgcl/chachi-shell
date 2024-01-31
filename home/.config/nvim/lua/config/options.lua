f182fe3910cc8de0a81913f4c8d998649a9cd635
Pablo Molina <contact@pablomolina.me>
2024-01-31 13:52:01 +0100

    neovim add liferay formatter

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -2,8 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false
-
 local opt = vim.opt
 
 opt.grepprg = "rg --vimgrep --hidden"
