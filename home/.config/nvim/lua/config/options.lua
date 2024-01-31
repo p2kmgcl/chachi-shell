b59edabd958bfd1c181ac8e85ff117326e2b53a0
Pablo Molina <contact@pablomolina.me>
2024-01-31 08:52:41 +0100

    neovim colorcolumn

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -9,3 +9,8 @@ local opt = vim.opt
 opt.grepprg = "rg --vimgrep --hidden"
 opt.undofile = false
 opt.swapfile = false
+
+-- Nice color column
+local colorcolumn = "80"
+for i=81,335 do colorcolumn = colorcolumn .. "," .. i end
+opt.colorcolumn = colorcolumn
