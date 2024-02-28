2ca56fe31136f39f34045b15b7307498bede4db1
Pablo Molina <contact@pablomolina.me>
2024-02-28 11:18:57 +0100

    javascript npx tldr man

--- home/.config/fish/conf.d/460-javascript.fish
+++ home/.config/fish/conf.d/460-javascript.fish
@@ -1,15 +1,17 @@
 if status is-interactive
-  if type -q $HOME/.deno/bin/deno
-    export DENO_INSTALL="$HOME/.deno"
-    fish_add_path $HOME/.deno/bin
-  else
-    echo -e "\e[33mdeno is not installed\e[0m"
-  end
+    if type -q $HOME/.deno/bin/deno
+        export DENO_INSTALL="$HOME/.deno"
+        fish_add_path $HOME/.deno/bin
+    else
+        echo -e "\e[33mdeno is not installed\e[0m"
+    end
 
-  if type -q $HOME/.fnm/fnm
-    fish_add_path $HOME/.fnm
-    fnm env --use-on-cd | source
-  else
-    echo -e "\e[33mfnm is not installed\e[0m"
-  end
+    if type -q $HOME/.fnm/fnm
+        fish_add_path $HOME/.fnm
+        fnm env --use-on-cd | source
+
+        alias man='npx -y tldr'
+    else
+        echo -e "\e[33mfnm is not installed\e[0m"
+    end
 end
