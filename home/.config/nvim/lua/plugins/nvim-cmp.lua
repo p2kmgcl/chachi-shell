67d807fe88bd11b458578e669a161a0e7dffddbd
Pablo Molina <contact@pablomolina.me>
2024-03-11 09:43:42 +0100

    nvim cmp no accept on enter

--- home/.config/nvim/lua/plugins/nvim-cmp.lua
+++ home/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,6 +3,20 @@ return {
   opts = function(_, opts)
     local cmp = require("cmp")
 
+    opts.mapping = cmp.mapping.preset.insert({
+      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+      ["<C-Y>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }),
+    })
+
     opts.sorting.comparators = {
       cmp.config.compare.offset, -- Entries with smaller offset will be ranked higher.
       cmp.config.compare.exact, -- Entries with exact == true will be ranked higher.
