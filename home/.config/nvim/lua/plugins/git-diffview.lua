93c081395924880e78a8fe12fafccd7666716870
Pablo Molina <contact@pablomolina.me>
2024-05-08 13:40:47 +0200

    feat(vim): neogit

--- home/.config/nvim/lua/plugins/git-diffview.lua
+++ home/.config/nvim/lua/plugins/git-diffview.lua
@@ -1,16 +1,12 @@
 return {
   {
     "sindrets/diffview.nvim",
+    event = "VeryLazy",
     keys = {
       {
         "<leader>dh",
         "<cmd>DiffviewFileHistory %<cr>",
         desc = "[D]ocument [h]istory",
-      },
-      {
-        "<leader>gs",
-        "<cmd>DiffviewOpen<cr>",
-        desc = "[G]it [s]tatus",
       },
     },
   },
---
+++ home/.config/nvim/lua/plugins/git-neogit.lua
@@ -0,0 +1,24 @@
+return {
+  "NeogitOrg/neogit",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  keys = {
+    {
+      "<leader>gs",
+      "<cmd>Neogit<cr>",
+      desc = "[G]it [s]tatus",
+    },
+  },
+  config = {
+    git_services = {
+      ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+      ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+      ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+      ["gitlab.protontech.ch"] = "https://gitlab.protontech.ch/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+    },
+  },
+}
