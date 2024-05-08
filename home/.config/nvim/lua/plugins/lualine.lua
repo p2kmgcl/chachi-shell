cc996f5eaa64f40430eae29372d293cc02a9a19e
Pablo Molina <contact@pablomolina.me>
2024-05-08 12:16:55 +0200

    feat(vim): lualine config

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -4,8 +4,56 @@ return {
     event = "VeryLazy",
     opts = function()
       return {
-        theme = "auto",
-        icons_enabled = true,
+        options = {
+          theme = "auto",
+          icons_enabled = true,
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = {},
+          lualine_c = {
+            {
+              "filename",
+              file_status = true,
+              newfile_status = true,
+              path = 1,
+
+              symbols = {
+                modified = "*️",
+                readonly = "🔒",
+                unnamed = "[No Name]",
+                newfile = "[New]",
+              },
+            },
+          },
+          lualine_x = { "filetype" },
+          lualine_y = { "diff", "diagnostics" },
+          lualine_z = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {
+            {
+              "filename",
+              file_status = true,
+              newfile_status = true,
+              path = 1,
+
+              symbols = {
+                modified = "*️",
+                readonly = "🔒",
+                unnamed = "[No Name]",
+                newfile = "[New]",
+              },
+            },
+          },
+          lualine_x = { "filetype" },
+          lualine_y = { "diff", "diagnostics" },
+          lualine_z = {},
+        },
       }
     end,
   },
