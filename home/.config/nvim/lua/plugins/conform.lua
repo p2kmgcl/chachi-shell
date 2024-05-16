b502991ca76f1fde200bdfccd1a0db0ea7bece37
Pablo Molina <contact@pablomolina.me>
2024-05-16 10:08:30 +0200

    fix(vim): conform js config

--- home/.config/nvim/lua/plugins/conform.lua
+++ home/.config/nvim/lua/plugins/conform.lua
@@ -20,7 +20,7 @@ return {
       -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
       return {
-        timeout_ms = 500,
+        timeout_ms = 1000,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
@@ -28,10 +28,12 @@ return {
       css = { "stylelint" },
       lua = { "stylua" },
       scss = { "stylelint" },
+      -- You can use a sub-list to tell conform to run *until* a formatter is found.
+      javascript = { { "eslint", "prettierd", "tsserver" } },
+      typescript = { { "eslint", "prettierd", "tsserver" } },
+      typescriptreact = { { "eslint", "prettierd", "tsserver" } },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
-      -- You can use a sub-list to tell conform to run *until* a formatter is found.
-      -- javascript = { { "prettierd", "prettier" } },
     },
   },
 }
