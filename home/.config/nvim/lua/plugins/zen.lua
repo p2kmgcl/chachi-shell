fb4127a4904638dae005a65f096622a558347a8e
Pablo Molina <contact@pablomolina.me>
2024-05-22 11:08:09 +0200

    feat(vim): zen mode

---
+++ home/.config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,43 @@
+return {
+  "folke/zen-mode.nvim",
+  lazy = true,
+  keys = {
+    {
+      "<leader>tz",
+      function()
+        require("zen-mode").toggle()
+      end,
+      mode = "",
+      desc = "[T]oggle [z]en mode",
+    },
+  },
+  opts = {
+    window = {
+      backdrop = 1,
+      width = 100,
+      height = 1,
+      options = {
+        signcolumn = "no",
+        number = false,
+        relativenumber = false,
+        cursorcolumn = false,
+        foldcolumn = "0",
+        list = false,
+      },
+    },
+    plugins = {
+      options = {
+        enabled = true,
+        ruler = false,
+        showcmd = false,
+        laststatus = 0,
+      },
+      gitsigns = { enabled = true },
+      tmux = { enabled = true },
+      alacritty = {
+        enabled = true,
+        font = "14",
+      },
+    },
+  },
+}
