c6cc151dfe4c878c8d2154d2143abc3a8fa2ed1f
Pablo Molina <contact@pablomolina.me>
2024-05-23 12:15:29 +0200

    chore(vim): noice defaults

--- home/.config/nvim/lua/plugins/noice.lua
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -1,7 +1,19 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    presets = {
+      command_palette = true, -- position the cmdline and popupmenu together
+      lsp_doc_border = true, -- add a border to hover docs and signature help
+    },
+  },
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
