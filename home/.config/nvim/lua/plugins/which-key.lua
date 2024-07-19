0c6e637421325411e220ad44f744f57e02b198ed
Pablo Molina <contact@pablomolina.me>
2024-07-19 10:56:04 +0200

    feat(vim): which-key update

--- home/.config/nvim/lua/plugins/which-key.lua
+++ home/.config/nvim/lua/plugins/which-key.lua
@@ -3,20 +3,26 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   config = function()
-    require("which-key").setup()
+    require("which-key").setup({
+      icons = {
+        mappings = false,
+      },
+    })
 
     -- Document existing key chains
-    require("which-key").register({
-      ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-      ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-      ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
-      ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-      ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-      ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+    require("which-key").add({
+      { "<leader>c", group = "[C]ode" },
+      { "<leader>c_", hidden = true },
+      { "<leader>d", group = "[D]ocument" },
+      { "<leader>d_", hidden = true },
+      { "<leader>f", group = "[F]ind" },
+      { "<leader>f_", hidden = true },
+      { "<leader>g", group = "[G]it" },
+      { "<leader>g_", hidden = true },
+      { "<leader>t", group = "[T]oggle" },
+      { "<leader>t_", hidden = true },
+      { "<leader>w", group = "[W]orkspace" },
+      { "<leader>w_", hidden = true },
     })
-    -- visual mode
-    require("which-key").register({
-      ["<leader>h"] = { "Git [H]unk" },
-    }, { mode = "v" })
   end,
 }
