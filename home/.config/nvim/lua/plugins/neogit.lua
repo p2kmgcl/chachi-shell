82c64965edc6131e2bc9c8f3d5cbb1d89105c709
Pablo Molina <contact@pablomolina.me>
2024-07-26 10:30:03 +0200

    feat(vim): neogit config

--- home/.config/nvim/lua/plugins/neogit.lua
+++ home/.config/nvim/lua/plugins/neogit.lua
@@ -6,7 +6,6 @@ return {
     "nvim-telescope/telescope.nvim",
   },
   lazy = true,
-  config = true,
   keys = {
     {
       "<leader>gc",
@@ -24,5 +23,17 @@ return {
       mode = "",
       desc = "[G]it [s]tatus",
     },
+  },
+  config = {
+    git_services = {
+      ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+      ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+      ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+      ["azure.com"] = "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
+      ["gitlab.protontech.ch"] = "https://gitlab.protontech.ch/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+    },
+    console_timeout = 2000,
+    auto_show_console = true,
+    auto_close_console = false,
   },
 }
