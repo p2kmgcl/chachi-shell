b122f1721402a55852077523c71d668d28083d4c
Pablo Molina <contact@pablomolina.me>
2024-11-08 21:08:48 +0100

    chore(vim): better completion

--- home/.config/nvim/lua/plugins/cmp.lua
+++ home/.config/nvim/lua/plugins/cmp.lua
@@ -3,37 +3,20 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    {
-      "L3MON4D3/LuaSnip",
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-          return
-        end
-        return "make install_jsregexp"
-      end)(),
-      dependencies = {},
-    },
-    "saadparwaiz1/cmp_luasnip",
-
-    -- Adds other completion capabilities.
-    --  nvim-cmp does not ship with all sources by default. They are split
-    --  into multiple repos for maintenance purposes.
+    "dcampos/nvim-snippy",
+    "dcampos/cmp-snippy",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
   },
   config = function()
     local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    luasnip.config.setup({})
+    local snippy = require("snippy")
+    snippy.setup({})
 
     cmp.setup({
       snippet = {
         expand = function(args)
-          luasnip.lsp_expand(args.body)
+          snippy.expand_snippet(args.body)
         end,
       },
       completion = {
@@ -44,23 +27,14 @@ return {
         documentation = {},
       },
 
-      -- For an understanding of why these mappings were
-      -- chosen, you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert({
-        -- Select the [n]ext item
         ["<C-n>"] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
         ["<C-p>"] = cmp.mapping.select_prev_item(),
 
         -- Scroll the documentation window [b]ack / [f]orward
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
         ["<C-e>"] = cmp.mapping.abort(),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Y>"] = cmp.mapping.confirm({
@@ -68,27 +42,22 @@ return {
           select = true,
         }),
 
-        -- Manually trigger a completion from nvim-cmp.
-        --  Generally you don't need this, because nvim-cmp will display
-        --  completions whenever it has completion options available.
         ["<C-Space>"] = cmp.mapping.complete({}),
 
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
         ["<C-l>"] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
+          if snippy.can_expand_or_advance() then
+            snippy.expand_or_advance()
           end
         end, { "i", "s" }),
         ["<C-h>"] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
+          if snippy.can_jump(-1) then
+            snippy.jump(-1)
           end
         end, { "i", "s" }),
       }),
       sources = {
         { name = "nvim_lsp" },
-        { name = "luasnip" },
+        { name = "snippy" },
         { name = "path" },
       },
     })
--- home/.config/nvim/lua/plugins/vim-visual-multi.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "mg979/vim-visual-multi",
-  version = "master",
-  event = "BufReadPost",
-  config = function() end,
-}
