ba5f27a052d57a5b2dc56c06c4ce898d119babdd
Pablo Molina <contact@pablomolina.me>
2024-11-08 11:24:30 +0100

    feat(vim): bash lsp

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -83,6 +83,7 @@ return {
     local nvim_lsp = require("lspconfig")
 
     local servers = {
+      bashls = {},
       denols = {
         root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
       },
