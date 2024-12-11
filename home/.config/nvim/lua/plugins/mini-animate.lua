135a49e3e292872f9cabe46ef1f4c45c3d8f0db6
Pablo Molina <contact@pablomolina.me>
2024-12-11 10:57:43 +0100

    chore(vim): mini-animate

--- home/.config/nvim/lua/plugins/mini-animate.lua
+++ home/.config/nvim/lua/plugins/mini-animate.lua
@@ -2,6 +2,19 @@ return {
   "echasnovski/mini.animate",
   event = "BufReadPost",
   config = function()
-    require("mini.animate").setup()
+    local animate = require("mini.animate")
+
+    animate.setup({
+      scroll = {
+        timing = animate.gen_timing.linear({
+          easing = "in-out",
+          duration = 10,
+          unit = "step",
+        }),
+      },
+      cursor = { enable = false },
+      open = { enable = false },
+      close = { enable = false },
+    })
   end,
 }
