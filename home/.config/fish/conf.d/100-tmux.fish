eaa72d32aa8799296b1d46d32ea4db2c47f52737
Pablo Molina <contact@pablomolina.me>
2025-01-07 10:56:46 +0100

    chore(fish): no tmux

--- home/.config/fish/conf.d/100-tmux.fish
+++ home/.config/fish/conf.d/100-tmux.fish
@@ -1,5 +1,5 @@
-if status is-interactive
-    if test -f "$(which tmux)" -a -z "$TMUX"
-        exec tmux new-session && exit 0
-    end
-end
+#if status is-interactive
+#    if test -f "$(which tmux)" -a -z "$TMUX"
+#        exec tmux new-session && exit 0
+#    end
+#end
