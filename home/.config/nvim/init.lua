7962b029c57de8959970ebb92bf7bee0cb17bb88
Pablo Molina <contact@pablomolina.me>
2025-02-18 14:16:24 +0100

    feat(vim): event better tab navigation

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -7,7 +7,7 @@ vim.g.maplocalleader = " "
 require("settings")
 require("keymaps")
 require("autocommands")
-require("terminaltabs")
+require("terminal")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
--- home/.config/nvim/lua/terminal.lua
+++ home/.config/nvim/lua/terminal.lua
@@ -1,17 +1,25 @@
 -- Exit terminal mode pressing Escape key
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Allow navigating outside of terminal tabs
+-- Navigating outside
 vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { silent = true })
 vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { silent = true })
 vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { silent = true })
 vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { silent = true })
 
--- Allow resizing terminal tabs
+-- Resizing
 vim.keymap.set("t", "<M-h>", "<C-\\><C-n><C-w><i", { silent = true })
 vim.keymap.set("t", "<M-j>", "<C-\\><C-n><C-w>-i", { silent = true })
 vim.keymap.set("t", "<M-k>", "<C-\\><C-n><C-w>+i", { silent = true })
 vim.keymap.set("t", "<M-l>", "<C-\\><C-n><C-w>>i", { silent = true })
+
+-- Scrolling
+vim.keymap.set("t", "<C-u>", "<C-\\><C-n><C-u>", { silent = true })
+vim.keymap.set("t", "<C-d>", "<C-\\><C-n><C-d>", { silent = true })
+
+-- Navigate back and forward
+vim.keymap.set("t", "<C-o>", "<C-\\><C-n><C-o>", { silent = true })
+vim.keymap.set("t", "<C-i>", "<C-\\><C-n><C-i>", { silent = true })
 
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "term://*",
