ed8283bba5ced24823540556da2c21407d13d730
Pablo Molina <pablo.molina@datadoghq.com>
2025-03-28 09:24:45 +0100

    chore(vim): review checkhealth

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -4,6 +4,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 require("settings")
 require("keymaps")
 require("autocommands")
