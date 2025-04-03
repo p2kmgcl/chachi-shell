7413d1070dc7156631d31f2ef42f40e88bf510de
Pablo Molina <pablo.molina@datadoghq.com>
2025-04-03 09:11:49 +0200

    transparent

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -11,15 +11,17 @@ if wezterm.gui then
 end
 if color_scheme:find("Dark") then
   config.color_scheme = "Catppuccin Mocha"
+  config.window_background_opacity = 0.9
 else
   config.color_scheme = "Catppuccin Latte"
+  config.window_background_opacity = 0.9
 end
 
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 config.enable_scroll_bar = false
-config.macos_window_background_blur = 10
+config.macos_window_background_blur = 50
 config.win32_system_backdrop = "Acrylic"
 config.window_padding = { top = 0, left = 0, right = 0, bottom = 0 }
 
