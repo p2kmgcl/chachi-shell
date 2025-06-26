d36d4ba7a5e3099f2eae053951c6920ead34b961
Pablo Molina <pablo.molina@datadoghq.com>
2025-06-26 14:31:15 +0200

    No cmp on enter

--- home/.config/nvim/lua/plugins/nvim-cmp.lua
+++ home/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,6 +1,11 @@
 return {
   "hrsh7th/nvim-cmp",
   opts = {
+    mapping = {
+      ["<CR>"] = function(fallback)
+        fallback()
+      end,
+    },
     experimental = {
       ghost_text = false,
     },
