e055b3282467554ff9f30595fc285a881ea96658
Pablo Molina <pablo.molina@datadoghq.com>
2025-07-03 10:04:05 +0200

    bufferline

---
+++ home/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  opts = {
+    options = {
+      max_name_length = 100,
+      max_prefix_length = 100,
+      truncate_names = false,
+      always_show_bufferline = true,
+      separator_style = "slant",
+    },
+  },
+}
