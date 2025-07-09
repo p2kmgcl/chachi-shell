9a49c295717acda6f95d190edd37d926322f41f7
Pablo Molina <pablo.molina@datadoghq.com>
2025-07-09 15:28:51 +0200

    ts tools

--- home/.config/nvim/lua/plugins/typescript-tools.lua
+++ home/.config/nvim/lua/plugins/typescript-tools.lua
@@ -1,10 +1,23 @@
 return {
   "pmizio/typescript-tools.nvim",
+  lazy = true,
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
   config = function()
     require("typescript-tools").setup({
       settings = {
+        separate_diagnostic_server = true,
+        publish_diagnostic_on = "insert_leave",
+        expose_as_code_action = "all",
+        tsserver_max_memory = "auto",
+        code_lens = "off",
+        disable_member_code_lens = true,
+        complete_function_calls = false,
+        include_completions_with_insert_text = false,
+        jsx_close_tag = {
+          enable = false,
+          filetypes = { "javascriptreact", "typescriptreact" },
+        },
         tsserver_file_preferences = {
           includeInlayParameterNameHints = "none",
           includeInlayParameterNameHintsWhenArgumentMatchesName = false,
@@ -18,11 +31,6 @@ return {
           allowIncompleteCompletions = false,
           allowRenameOfImportPath = false,
         },
-        tsserver_max_memory = 30720,
-        complete_function_calls = false,
-        include_completions_with_insert_text = false,
-        code_lens = "off",
-        disable_member_code_lens = true,
       },
     })
   end,
