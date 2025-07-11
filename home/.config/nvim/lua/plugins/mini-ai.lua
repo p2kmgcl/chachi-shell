1efe3cae9e52f585500f3b41cc46d3e9d83a1b2c
Pablo Molina <pablo.molina@datadoghq.com>
2025-07-11 11:21:11 +0200

    mini ai assignment

---
+++ home/.config/nvim/lua/plugins/mini-ai.lua
@@ -0,0 +1,15 @@
+return {
+  "echasnovski/mini.ai",
+  opts = function()
+    local ai = require("mini.ai")
+
+    return {
+      custom_textobjects = {
+        a = ai.gen_spec.treesitter({
+          a = { "@parameter.outer", "@attribute.outer", "@assignment.outer" },
+          i = { "@parameter.inner", "@attribute.inner", "@assignment.inner" },
+        }),
+      },
+    }
+  end,
+}
