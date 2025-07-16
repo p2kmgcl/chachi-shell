8778501d30c925a4b77e204962dd1faa256e5a98
Pablo Molina <pablo.molina@datadoghq.com>
2025-07-16 12:16:49 +0200

    nicer gitsigns

--- home/.config/nvim/lua/common/plugins/gitsigns.lua
+++ home/.config/nvim/lua/common/plugins/gitsigns.lua
@@ -1,12 +1,10 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
-    update_debounce = 10000,
     watch_gitdir = {
       interval = 0,
       follow_files = false,
     },
-    max_file_length = 40000,
     preview_config = {
       border = "single",
       style = "minimal",
@@ -16,36 +14,13 @@ return {
     },
   },
   config = function(_, opts)
-    require("gitsigns").setup(opts)
+    local gitsigns = require("gitsigns")
+    gitsigns.setup(opts)
 
     local group = vim.api.nvim_create_augroup("GitsignsRefresh", { clear = true })
-
-    vim.api.nvim_create_autocmd("InsertLeave", {
-      group = group,
-      callback = function()
-        require("gitsigns").refresh()
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      group = group,
-      callback = function()
-        require("gitsigns").refresh()
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("BufEnter", {
-      group = group,
-      callback = function()
-        require("gitsigns").refresh()
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("FocusGained", {
-      group = group,
-      callback = function()
-        require("gitsigns").refresh()
-      end,
-    })
+    vim.api.nvim_create_autocmd("InsertLeave", { group = group, callback = gitsigns.refresh })
+    vim.api.nvim_create_autocmd("BufWritePost", { group = group, callback = gitsigns.refresh })
+    vim.api.nvim_create_autocmd("BufEnter", { group = group, callback = gitsigns.refresh })
+    vim.api.nvim_create_autocmd("FocusGained", { group = group, callback = gitsigns.refresh })
   end,
 }
