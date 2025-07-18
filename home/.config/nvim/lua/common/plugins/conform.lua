42b949e3e4bbd8eb8615716a0541b6dffd7b7ab7
Pablo Molina <pablo.molina@datadoghq.com>
2025-07-18 12:55:18 +0200

    prefer eslint format

--- home/.config/nvim/lua/common/plugins/conform.lua
+++ home/.config/nvim/lua/common/plugins/conform.lua
@@ -7,7 +7,22 @@ return {
     {
       "<leader>cf",
       function()
-        require("conform").format()
+        local has_eslint = false
+        for _, client in ipairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+          if client.name == "eslint-lsp" then
+            has_eslint = true
+          end
+        end
+
+        if has_eslint then
+          vim.lsp.buf.execute_command({
+            command = "eslint.applyAllFixes",
+            arguments = { { uri = vim.uri_from_bufnr(0), version = vim.lsp.util.buf_versions[0] } },
+          })
+        else
+          local conform = require("conform")
+          conform.format()
+        end
       end,
       mode = "n",
       desc = "Format buffer",
@@ -27,7 +42,11 @@ return {
         lsp_format = "fallback",
       },
       formatters = {
-        injected = { options = { ignore_errors = true } },
+        injected = {
+          options = {
+            ignore_errors = true,
+          },
+        },
       },
       formatters_by_ft = formatters_by_ft,
     }
