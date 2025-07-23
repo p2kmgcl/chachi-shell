402147918cc8738b4d8641ed1172ef033453c7b2
Pablo Molina <pablo.molina@datadoghq.com>
2025-07-23 09:48:59 +0200

    no tiny code action

--- home/.config/nvim/lua/common/setup/lsp.lua
+++ home/.config/nvim/lua/common/setup/lsp.lua
@@ -1,12 +1,33 @@
-local methods = vim.lsp.protocol.Methods
+vim.keymap.set({ "n" }, "<leader>ll", function()
+  for _, client in pairs(vim.lsp.get_active_clients()) do
+    print("LSP:", client.name)
+    local cmds = client.server_capabilities.executeCommandProvider
+    if cmds and cmds.commands then
+      for _, cmd in ipairs(cmds.commands) do
+        print("  - " .. cmd)
+      end
+    else
+      print("  (no executeCommandProvider)")
+    end
+  end
+  vim.cmd('messages')
+end, {desc = "List available commands" })
 
 local function on_attach(client, bufnr)
-  vim.keymap.set({ "n", "x" }, "gra", function()
-    local tiny_code_action = require("tiny-code-action")
-    tiny_code_action.code_action({ bufnr = bufnr })
-  end, { desc = "vim.lsp.buf.code_action()", buffer = bufnr })
+  -- Native keymap
+  -- vim.keymap.set({ "n", "x" }, "grA", function() end)
 
-  if client:supports_method(methods.textDocument_signatureHelp) then
+  vim.keymap.set({ "n", "x" }, "grA", function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source" },
+        diagnostics = {},
+      },
+    })
+  end, { desc = 'vim.lsp.buf.code_action("source")', buffer = bufnr })
+
+  if client:supports_method(vim.lsp.protocol.Methods.textDocument_signatureHelp) then
     vim.keymap.set("i", "<C-k>", function()
       if require("blink.cmp.completion.windows.menu").win:is_open() then
         require("blink.cmp").hide()
@@ -15,15 +36,15 @@ local function on_attach(client, bufnr)
     end, { desc = "Signature help", buffer = bufnr })
   end
 
-  if client:supports_method(methods.textDocument_inlayHint) then
+  if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
     vim.defer_fn(function()
       vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
     end, 500)
   end
 end
 
-local register_capability = vim.lsp.handlers[methods.client_registerCapability]
-vim.lsp.handlers[methods.client_registerCapability] = function(err, res, ctx)
+local register_capability = vim.lsp.handlers[vim.lsp.protocol.Methods.client_registerCapability]
+vim.lsp.handlers[vim.lsp.protocol.Methods.client_registerCapability] = function(err, res, ctx)
   local client = vim.lsp.get_client_by_id(ctx.client_id)
   if client then
     on_attach(client, vim.api.nvim_get_current_buf())
