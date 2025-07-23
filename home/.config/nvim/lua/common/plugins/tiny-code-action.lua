94b896c1a0619298def5b52ac0ba9e6a5c7057ae
Pablo Molina <pablo.molina@datadoghq.com>
2025-07-23 08:34:51 +0200

    tiny code action tweaks

--- home/.config/nvim/lua/common/plugins/tiny-code-action.lua
+++ home/.config/nvim/lua/common/plugins/tiny-code-action.lua
@@ -1,9 +1,15 @@
 return {
   "rachartier/tiny-code-action.nvim",
-  dependencies = {
-    { "nvim-lua/plenary.nvim" },
-    { "folke/snacks.nvim", opts = { terminal = {} } },
-  },
+  dependencies = { { "nvim-lua/plenary.nvim" } },
   event = "LspAttach",
-  opts = { picker = "snacks" },
+  opts = {
+    picker = {
+      "buffer",
+      opts = {
+        hotkeys = true,
+        hotkeys_mode = "text_based",
+        auto_preview = false,
+      },
+    },
+  },
 }
