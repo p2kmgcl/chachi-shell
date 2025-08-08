8bb668654b1ece5f7535e1284d98abf4b2cc2430
Pablo Molina <pablo.molina@datadoghq.com>
2025-08-08 16:56:20 +0200

    Clean flash

--- home/.config/nvim/lua/plugins/flash.lua
+++ home/.config/nvim/lua/plugins/flash.lua
@@ -10,38 +10,6 @@ return {
       end,
       desc = "Flash",
     },
-    {
-      "S",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").treesitter()
-      end,
-      desc = "Flash Treesitter",
-    },
-    {
-      "r",
-      mode = "o",
-      function()
-        require("flash").remote()
-      end,
-      desc = "Remote Flash",
-    },
-    {
-      "R",
-      mode = { "o", "x" },
-      function()
-        require("flash").treesitter_search()
-      end,
-      desc = "Treesitter Search",
-    },
-    {
-      "<c-s>",
-      mode = { "c" },
-      function()
-        require("flash").toggle()
-      end,
-      desc = "Toggle Flash Search",
-    },
   },
   opts = function()
     vim.api.nvim_set_hl(0, "FlashMatch", { underline = true })
