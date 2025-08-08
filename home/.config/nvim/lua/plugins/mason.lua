b0024733c4436cca575b37ddf7890907b6f5f367
Pablo Molina <pablo.molina@datadoghq.com>
2025-08-08 16:05:11 +0200

    Update mason package resolver

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -26,17 +26,21 @@ return {
     }
 
     local registry = require("mason-registry")
-    for _, mason_name in ipairs(registry.get_all_package_names()) do
-      if vim.tbl_contains(enabled_packages, mason_name) then
-        if not registry.is_installed(mason_name) then
-          vim.notify("Installing package " .. mason_name, vim.log.levels.INFO, { title = "Mason" })
-          registry.get_package(mason_name):install()
+    local all_package_names = registry.get_all_package_names()
+    for _, package_name in ipairs(enabled_packages) do
+      if vim.tbl_contains(all_package_names, package_name) then
+        if not registry.is_installed(package_name) then
+          vim.notify("Installing package " .. package_name, vim.log.levels.INFO, { title = "Mason" })
+          registry.get_package(package_name):install()
         end
       else
-        if registry.is_installed(mason_name) then
-          vim.notify("Uninstalling package " .. mason_name, vim.log.levels.INFO, { title = "Mason" })
-          registry.get_package(mason_name):uninstall()
-        end
+        error("Package " .. package_name .. " is required but not found")
+      end
+    end
+    for _, mason_name in ipairs(all_package_names) do
+      if not vim.tbl_contains(enabled_packages, mason_name) and registry.is_installed(mason_name) then
+        vim.notify("Uninstalling package " .. mason_name, vim.log.levels.INFO, { title = "Mason" })
+        registry.get_package(mason_name):uninstall()
       end
     end
   end,
