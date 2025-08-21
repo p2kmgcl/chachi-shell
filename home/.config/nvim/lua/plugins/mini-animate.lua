bb189d728b8cd918c0bf51212ef51f9620a73457
Pablo Molina <pablo.molina@datadoghq.com>
2025-08-21 09:16:24 +0200

    Moar animations

--- home/.config/nvim/lua/plugins/mini-animate.lua
+++ home/.config/nvim/lua/plugins/mini-animate.lua
@@ -41,7 +41,7 @@ return {
         timing = animate.gen_timing.linear({ duration = 50, unit = "total" }),
       },
       scroll = {
-        timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+        timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
         subscroll = animate.gen_subscroll.equal({
           predicate = function(total_scroll)
             if mouse_scrolled then
---
+++ home/.config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,13 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    legacy_computing_symbols_support = true,
+    stiffness = 0.8,
+    trailing_stiffness = 0.6,
+    stiffness_insert_mode = 0.7,
+    trailing_stiffness_insert_mode = 0.7,
+    damping = 0.95,
+    damping_insert_mode = 0.95,
+    distance_stop_animating = 0.5,
+  },
+}
