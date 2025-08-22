f3fc52b40fb8b57d4a86234dc3205f1decc7e479
Pablo Molina <pablo.molina@datadoghq.com>
2025-08-22 13:34:45 +0200

    Smearer

--- home/.config/nvim/lua/plugins/smear-cursor.lua
+++ home/.config/nvim/lua/plugins/smear-cursor.lua
@@ -1,13 +1,18 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
+    smear_insert_mode = false,
     legacy_computing_symbols_support = true,
-    stiffness = 0.8,
-    trailing_stiffness = 0.6,
-    stiffness_insert_mode = 0.7,
-    trailing_stiffness_insert_mode = 0.7,
-    damping = 0.95,
-    damping_insert_mode = 0.95,
-    distance_stop_animating = 0.5,
+
+    hide_target_hack = true,
+    never_draw_over_target = true,
+
+    min_horizontal_distance_smear = 4,
+    min_vertical_distance_smear = 10,
+
+    damping = 1.0,
+    stiffness = 0.6,
+    trailing_stiffness = 0.4,
+    matrix_pixel_threshold = 0.5,
   },
 }
