6e865b833ef69bc407d57d262b14fe4f62f1cd4d
Pablo Molina <contact@pablomolina.me>
2025-09-08 19:49:28 +0200

    Auto dark mode

---
+++ home/.config/nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,17 @@
+return {
+  "f-person/auto-dark-mode.nvim",
+  dependencies = {
+    "projekt0n/github-nvim-theme",
+    "akinsho/bufferline.nvim",
+  },
+  opts = {
+    set_dark_mode = function()
+      vim.cmd("colorscheme github_dark_colorblind")
+    end,
+    set_light_mode = function()
+      vim.cmd("colorscheme github_light_colorblind")
+    end,
+    update_interval = 3000,
+    fallback = "dark",
+  },
+}
