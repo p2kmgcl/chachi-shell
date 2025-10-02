83f2fa21dd2b220d302682646e273806e3458ff6
Pablo Molina <pablo.molina@datadoghq.com>
2025-10-02 12:54:04 +0200

    feat(nvim): split resizing

--- home/.config/nvim/lua/keymaps/window-management.lua
+++ home/.config/nvim/lua/keymaps/window-management.lua
@@ -3,7 +3,29 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = tr
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
-vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+local resize = function(key)
+  local amount = 10
+
+  vim.keymap.set("n", "<M-" .. key .. ">", function()
+    local win = vim.api.nvim_get_current_win()
+    if key == "h" then
+      vim.cmd("wincmd h")
+      vim.cmd("vertical resize -" .. amount)
+    elseif key == "j" then
+      vim.cmd("wincmd k")
+      vim.cmd("resize +" .. amount)
+    elseif key == "k" then
+      vim.cmd("wincmd k")
+      vim.cmd("resize -" .. amount)
+    elseif key == "l" then
+      vim.cmd("wincmd h")
+      vim.cmd("vertical resize +" .. amount)
+    end
+    vim.api.nvim_set_current_win(win)
+  end)
+end
+
+resize("h")
+resize("j")
+resize("k")
+resize("l")
