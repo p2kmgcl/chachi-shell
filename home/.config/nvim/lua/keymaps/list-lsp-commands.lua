13fd32250a0f6b563ce925646d207d369fc46dec
Pablo Molina <pablo.molina@datadoghq.com>
2025-12-16 18:05:07 +0100

    feat(nvim): implement lsp commands

--- home/.config/nvim/lua/keymaps/list-lsp-commands.lua
+++ home/.config/nvim/lua/keymaps/list-lsp-commands.lua
@@ -1,14 +1,36 @@
 vim.keymap.set({ "n" }, "<leader>ll", function()
-  for _, client in pairs(vim.lsp.get_active_clients()) do
-    print("LSP:", client.name)
+  local commands = {}
+
+  for _, client in pairs(vim.lsp.get_clients()) do
     local cmds = client.server_capabilities.executeCommandProvider
     if cmds and cmds.commands then
       for _, cmd in ipairs(cmds.commands) do
-        print("  - " .. cmd)
+        table.insert(commands, {
+          command = cmd,
+          client_name = client.name,
+          client_id = client.id,
+        })
       end
-    else
-      print("  (no executeCommandProvider)")
     end
   end
-  vim.cmd("messages")
-end, { desc = "List available commands" })
+
+  if #commands == 0 then
+    vim.notify("No LSP commands available", vim.log.levels.WARN)
+    return
+  end
+
+  vim.ui.select(commands, {
+    prompt = "Select LSP Command:",
+    format_item = function(item)
+      return string.format("%s: %s", item.client_name, item.command)
+    end,
+  }, function(selected)
+    if not selected then
+      return
+    end
+
+    vim.lsp.buf.execute_command({
+      command = selected.command,
+    })
+  end)
+end, { desc = "LSP Commands" })
