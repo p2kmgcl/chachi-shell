9cd2f4ccba5438d804501c542c741a362776990b
Pablo Molina <pablo.molina@datadoghq.com>
2025-12-16 17:06:37 +0100

    feat(nvim): configure fidget for LSP progress notifications

--- home/.config/nvim/lua/plugins/fidget.lua
+++ home/.config/nvim/lua/plugins/fidget.lua
@@ -1,8 +1,64 @@
 return {
   "j-hui/fidget.nvim",
   opts = {
+    progress = {
+      poll_rate = 0,
+      suppress_on_insert = false,
+      ignore_done_already = false,
+      ignore_empty_message = false,
+      
+      notification_group = function(msg)
+        return msg.lsp_client.name
+      end,
+      
+      display = {
+        render_limit = 20,
+        done_ttl = 3,
+        done_icon = "✔︎",
+        done_style = "Constant",
+        progress_ttl = math.huge,
+        progress_icon = { pattern = "dots", period = 1 },
+        progress_style = "WarningMsg",
+        group_style = "Title",
+        icon_style = "Question",
+        priority = 30,
+        skip_history = true,
+      },
+      
+      lsp = {
+        progress_ringbuf_size = 100,
+        log_handler = false,
+      },
+    },
+    
     notification = {
       override_vim_notify = true,
+      poll_rate = 10,
+      filter = vim.log.levels.INFO,
+      history_size = 256,
+      view = {
+        stack_upwards = true,
+        icon_separator = " ",
+        group_separator = "---",
+        group_separator_hl = "Comment",
+      },
+      window = {
+        normal_hl = "Comment",
+        winblend = 100,
+        border = "none",
+        zindex = 45,
+        max_width = 0,
+        max_height = 0,
+        x_padding = 1,
+        y_padding = 0,
+        align = "bottom",
+        relative = "editor",
+      },
+    },
+    
+    logger = {
+      level = vim.log.levels.WARN,
+      max_size = 10000,
     },
   },
 }
