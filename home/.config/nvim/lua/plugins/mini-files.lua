a5c0891ae461a42aaf5c55f1f758adea7b8e287f
Pablo Molina <pablo.molina@datadoghq.com>
2025-12-18 11:20:19 +0100

    feat(nvim): open in splits with mini files

--- home/.config/nvim/lua/plugins/mini-files.lua
+++ home/.config/nvim/lua/plugins/mini-files.lua
@@ -29,4 +29,30 @@ return {
     { "<leader>fm", false },
     { "<leader>fM", false },
   },
+  config = function(_, opts)
+    local mini_files = require("mini.files")
+    mini_files.setup(opts)
+
+    local map_split = function(buf_id, lhs, direction)
+      local rhs = function()
+        local entry_path = mini_files.get_fs_entry().path
+        local is_at_file = vim.fn.filereadable(entry_path) == 1
+        mini_files.close()
+        vim.cmd(direction .. " " .. vim.fn.fnameescape(entry_path))
+      end
+
+      vim.keymap.set("n", lhs, rhs, {
+        buffer = buf_id,
+        desc = "Open in " .. direction,
+      })
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        map_split(args.data.buf_id, "<C-s>", "split")
+        map_split(args.data.buf_id, "<C-v>", "vsplit")
+      end,
+    })
+  end,
 }
