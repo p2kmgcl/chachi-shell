b2aa75f12f272748f813f776d61e4b854323fbe9
Pablo Molina <pablo.molina@datadoghq.com>
2026-03-09 09:45:26 +0100

    feat(nvim): use built in TS incremental selection
    https://github.com/MariaSolOs/dotfiles/commit/a0e0f074093ec8b2e9115b3611ccfb3e5ec91d9d

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -54,15 +54,6 @@ return {
     },
     highlight = { enable = true },
     indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<cr>",
-        node_incremental = "<cr>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
     textobjects = {
       select = {
         enable = true,
