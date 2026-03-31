f3fd5328f938c710dd1c3419354280bdd64997fc
Pablo Molina <pablo.molina@datadoghq.com>
2026-03-31 12:37:07 +0200

    fix(nvim): suppress blocking LSP NO_RESULT_CALLBACK_FOUND errors

--- home/.config/nvim/lua/setup/lsp-setup.lua
+++ home/.config/nvim/lua/setup/lsp-setup.lua
@@ -21,3 +21,24 @@ vim.lsp.enable({
 vim.lsp.config("*", {
   capabilities = vim.lsp.protocol.make_client_capabilities(),
 })
+
+-- Patch LSP clients to suppress NO_RESULT_CALLBACK_FOUND from blocking the UI.
+-- The default write_error uses nvim_echo with err=true which triggers "Press ENTER".
+-- We override it to route that specific error to vim.notify (picked up by fidget).
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client or client._patched_write_error then
+      return
+    end
+    local original = client.write_error
+    client.write_error = function(self, code, err)
+      if code == vim.lsp.rpc.client_errors.NO_RESULT_CALLBACK_FOUND then
+        vim.notify("LSP[" .. self.name .. "]: NO_RESULT_CALLBACK_FOUND", vim.log.levels.WARN)
+        return
+      end
+      original(self, code, err)
+    end
+    client._patched_write_error = true
+  end,
+})
