08ee02ce7e60df3b6e443a5bd3b06939e0cc4796
Pablo Molina <pablo.molina@datadoghq.com>
2026-04-15 13:49:01 +0200

    feat(nvim): delete all buffers

--- home/.config/nvim/lua/keymaps/delete-all-buffers.lua
+++ home/.config/nvim/lua/keymaps/delete-all-buffers.lua
@@ -1,11 +1,10 @@
-vim.keymap.set("n", "<leader>bo", function()
-  local current_buf = vim.api.nvim_get_current_buf()
+vim.keymap.set("n", "<leader>ba", function()
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if buf ~= current_buf and vim.api.nvim_buf_is_valid(buf) then
+    if vim.api.nvim_buf_is_valid(buf) then
       local buf_type = vim.api.nvim_get_option_value("buftype", { buf = buf })
       if buf_type ~= "terminal" then
         vim.api.nvim_buf_delete(buf, { force = false })
       end
     end
   end
-end, { desc = "Delete Other Buffers" })
+end, { desc = "Delete All Buffers" })
