e605c09cc3555f2fd77c1e04cce07f36282fd632
Pablo Molina <pablo.molina@datadoghq.com>
2026-06-08 09:16:04 +0200

    feat(nvim): better a-side icons

--- home/.config/nvim/lua/a-side/ui/tree/annotations.lua
+++ home/.config/nvim/lua/a-side/ui/tree/annotations.lua
@@ -1,21 +1,21 @@
 local M = {}
 
 local DEFS = {
-  { id = 'index_added',         text = '+', hl = 'DiffAdd' },
-  { id = 'index_modified',      text = '•', hl = 'DiffAdd' },
-  { id = 'index_deleted',       text = '⨯', hl = 'DiffAdd' },
-  { id = 'index_renamed',       text = '↦', hl = 'DiffAdd' },
-  { id = 'index_copied',        text = '⇔', hl = 'DiffAdd' },
-  { id = 'index_typechange',    text = '⇄', hl = 'DiffAdd' },
-  { id = 'worktree_modified',   text = '•', hl = 'Comment' },
-  { id = 'worktree_deleted',    text = '⨯', hl = 'Comment' },
-  { id = 'worktree_typechange', text = '⇆', hl = 'Comment' },
-  { id = 'untracked',           text = '?', hl = 'Comment' },
-  { id = 'ignored',             text = '◌', hl = 'Comment' },
-  { id = 'conflicted',          text = '!', hl = 'Comment' },
-  { id = 'buffer_modified',     text = '•', hl = 'Comment' },
-  { id = 'clipboard_cut',       text = '📎', hl = 'Comment' },
-  { id = 'clipboard_copy',      text = '📎', hl = 'Comment' },
+  { id = 'index_added',         text = '', hl = 'DiffAdd' },
+  { id = 'index_modified',      text = '', hl = 'DiffAdd' },
+  { id = 'index_deleted',       text = '', hl = 'DiffAdd' },
+  { id = 'index_renamed',       text = '󰬲', hl = 'DiffAdd' },
+  { id = 'index_copied',        text = '󰈢', hl = 'DiffAdd' },
+  { id = 'index_typechange',    text = '', hl = 'DiffAdd' },
+  { id = 'worktree_modified',   text = '󰏭', hl = 'Comment' },
+  { id = 'worktree_deleted',    text = '', hl = 'Comment' },
+  { id = 'worktree_typechange', text = '', hl = 'Comment' },
+  { id = 'untracked',           text = '󰡯', hl = 'Comment' },
+  { id = 'ignored',             text = '󰘓', hl = 'Comment' },
+  { id = 'conflicted',          text = '󰀧', hl = 'Comment' },
+  { id = 'buffer_modified',     text = '', hl = 'Comment' },
+  { id = 'clipboard_cut',       text = '', hl = 'Comment' },
+  { id = 'clipboard_copy',      text = '', hl = 'Comment' },
 }
 
 local by_id = {}
